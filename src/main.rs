6f7c70e204746bd66a82fc105edae723453d3968
trynaeat <jamesparks10@gmail.com>
2020-02-13 21:37:27 -0500

    Vastly improved smoothness of movement

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::rect::Point;
 use std::time::Duration;
+use std::collections::HashSet;
 
 use cgmath::Vector2;
 use cgmath::Vector3;
@@ -14,8 +15,14 @@ use cgmath::Vector3;
 const SCREEN_WIDTH: i32 = 1920;
 const SCREEN_HEIGHT: i32 = 1080;
 const WALL_HEIGHT_SCALE: i32 = 1;
-const MOVE_SPEED: f64 = 6.0;
-const ROT_SPEED: f64 = 3.0;
+const MOVE_SPEED: f64 = 4.0;
+const ROT_SPEED: f64 = 2.0;
+
+struct Player {
+    pos: Vector3<f64>,
+    dir: Vector2<f64>,
+    camera_plane: Vector2<f64>
+}
 
 struct MapGrid {
     x: i32,
@@ -31,9 +38,11 @@ pub fn main() {
     // Init map
     let world_map = crate::data::load_map("./src/data/maps/map.json");
     // Init Player and Camera
-    let mut player_pos = Vector3::new(22.0, 12.0, 0.0);
-    let mut player_dir = Vector2::new(-1.0, 0.0);
-    let mut camera_plane = Vector2::new(0.0, 0.66);
+    let mut player = Player {
+        pos: Vector3::new(22.0, 12.0, 0.0),
+        dir: Vector2::new(-1.0, 0.0),
+        camera_plane: Vector2::new(0.0, 0.66)
+    };
     // SDL setup and loop
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -59,10 +68,10 @@ pub fn main() {
         for i in 0..SCREEN_WIDTH {
             // Calculate incoming ray position/direction
             let camera_x: f64 = 2.0 * i as f64 / SCREEN_WIDTH as f64 - 1.0;
-            let ray_hit_pos = camera_x * camera_plane;
-            let ray_dir = player_dir + ray_hit_pos;
+            let ray_hit_pos = camera_x * player.camera_plane;
+            let ray_dir = player.dir + ray_hit_pos;
             // Which box we're in
-            let mut curr_grid = MapGrid { x: player_pos.x as i32, y: player_pos.y as i32 };
+            let mut curr_grid = MapGrid { x: player.pos.x as i32, y: player.pos.y as i32 };
             // Length of ray from any x/y side to next x/y side
             let delta_dist = Vector2::new((1.0 / ray_dir.x).abs(), (1.0 / ray_dir.y).abs());
             let step_x: i32;
@@ -71,17 +80,17 @@ pub fn main() {
             let mut side_dist_y: f64;
             if ray_dir.x < 0.0 {
                 step_x = -1;
-                side_dist_x = (player_pos.x - curr_grid.x as f64) * delta_dist.x;
+                side_dist_x = (player.pos.x - curr_grid.x as f64) * delta_dist.x;
             } else {
                 step_x = 1;
-                side_dist_x = (curr_grid.x as f64 + 1.0 - player_pos.x) * delta_dist.x;
+                side_dist_x = (curr_grid.x as f64 + 1.0 - player.pos.x) * delta_dist.x;
             }
             if ray_dir.y < 0.0 {
                 step_y = -1;
-                side_dist_y = (player_pos.y - curr_grid.y as f64) * delta_dist.y;
+                side_dist_y = (player.pos.y - curr_grid.y as f64) * delta_dist.y;
             } else {
                 step_y = 1;
-                side_dist_y = (curr_grid.y as f64 + 1.0 - player_pos.y) * delta_dist.y;
+                side_dist_y = (curr_grid.y as f64 + 1.0 - player.pos.y) * delta_dist.y;
             }
 
             // start DDA
@@ -101,8 +110,8 @@ pub fn main() {
                 }
             }
             let perp_wall_dist = match side {
-                WallSide::X => (curr_grid.x as f64 - player_pos.x + (1.0 - step_x as f64) / 2.0) / ray_dir.x,
-                WallSide::Y => (curr_grid.y as f64 - player_pos.y + (1.0 - step_y as f64) / 2.0) / ray_dir.y,
+                WallSide::X => (curr_grid.x as f64 - player.pos.x + (1.0 - step_x as f64) / 2.0) / ray_dir.x,
+                WallSide::Y => (curr_grid.y as f64 - player.pos.y + (1.0 - step_y as f64) / 2.0) / ray_dir.y,
             };
             // Calculate height of line
             let line_height = (WALL_HEIGHT_SCALE as f64 * SCREEN_HEIGHT as f64 / perp_wall_dist) as i32;
@@ -134,56 +143,13 @@ pub fn main() {
         let frame_time = (time - old_time) as f64 / 1000.0; // in seconds
         old_time = time;
         draw_fps(frame_time);
-
-        // Todo properly encapsulate inputs and make this a function
-        let mut read_player_input = |keycode: Keycode, frame_time: f64| {
-            let move_speed = frame_time * MOVE_SPEED;
-            let rot_speed = frame_time * ROT_SPEED;
-            match keycode {
-                Keycode::Up => {
-                    let new_pos = player_pos + Vector3::new(player_dir.x * move_speed, player_dir.y * move_speed, 0.0);
-                    if world_map[new_pos.x as usize][new_pos.y as usize] == 0 {
-                        player_pos = new_pos;
-                    }
-                },
-                Keycode::Down => {
-                    let new_pos = player_pos - Vector3::new(player_dir.x * move_speed, player_dir.y * move_speed, 0.0);
-                    if world_map[new_pos.x as usize][new_pos.y as usize] == 0 {
-                        player_pos = new_pos;
-                    }
-                },
-                Keycode::Right => {
-                    player_dir = Vector2::new(
-                        player_dir.x * (-rot_speed).cos() - player_dir.y * (-rot_speed).sin(),
-                        player_dir.x * (-rot_speed).sin() + player_dir.y * (-rot_speed).cos(),
-                    );
-                    camera_plane = Vector2::new(
-                        camera_plane.x * (-rot_speed).cos() - camera_plane.y * (-rot_speed).sin(),
-                        camera_plane.x * (-rot_speed).sin() + camera_plane.y * (-rot_speed).cos(),
-                    );
-                },
-                Keycode::Left => {
-                    player_dir = Vector2::new(
-                        player_dir.x * rot_speed.cos() - player_dir.y * rot_speed.sin(),
-                        player_dir.x * rot_speed.sin() + player_dir.y * rot_speed.cos(),
-                    );
-                    camera_plane = Vector2::new(
-                        camera_plane.x * rot_speed.cos() - camera_plane.y * rot_speed.sin(),
-                        camera_plane.x * rot_speed.sin() + camera_plane.y * rot_speed.cos(),
-                    );
-                }
-                _ => (),
-            }
-        };
+        move_player(&mut player, &world_map, &event_pump, frame_time);
 
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running
-                },
-                Event::KeyDown { keycode: Some(keycode), .. } => {
-                    read_player_input(keycode, frame_time);
                 },
                 _ => {}
             }
@@ -196,5 +162,46 @@ pub fn main() {
     pub fn draw_fps(frame_time: f64) {
         // TODO: draw text
         // println!("{}", 1.0 / frame_time);
+    }
+
+    fn move_player (player: &mut Player, world_map: &[[u32; 24]; 24], event_pump: &sdl2::EventPump, frame_time: f64) {
+        let move_speed = frame_time * MOVE_SPEED;
+        let rot_speed = frame_time * ROT_SPEED;
+        let pressed_keys: HashSet<Keycode> = event_pump.keyboard_state()
+            .pressed_scancodes()
+            .filter_map(Keycode::from_scancode)
+            .collect();
+        if pressed_keys.contains(&Keycode::Up) {
+            let new_pos = player.pos + Vector3::new(player.dir.x * move_speed, player.dir.y * move_speed, 0.0);
+            if world_map[new_pos.x as usize][new_pos.y as usize] == 0 {
+                player.pos = new_pos;
+            }
+        }
+        if pressed_keys.contains(&Keycode::Down) {
+            let new_pos = player.pos - Vector3::new(player.dir.x * move_speed, player.dir.y * move_speed, 0.0);
+            if world_map[new_pos.x as usize][new_pos.y as usize] == 0 {
+                player.pos = new_pos;
+            }
+        }
+        if pressed_keys.contains(&Keycode::Left) {
+            player.dir = Vector2::new(
+                player.dir.x * rot_speed.cos() - player.dir.y * rot_speed.sin(),
+                player.dir.x * rot_speed.sin() + player.dir.y * rot_speed.cos(),
+            );
+            player.camera_plane = Vector2::new(
+                player.camera_plane.x * rot_speed.cos() - player.camera_plane.y * rot_speed.sin(),
+                player.camera_plane.x * rot_speed.sin() + player.camera_plane.y * rot_speed.cos(),
+            );
+        }
+        if pressed_keys.contains(&Keycode::Right) {
+            player.dir = Vector2::new(
+                player.dir.x * (-rot_speed).cos() - player.dir.y * (-rot_speed).sin(),
+                player.dir.x * (-rot_speed).sin() + player.dir.y * (-rot_speed).cos(),
+            );
+            player.camera_plane = Vector2::new(
+                player.camera_plane.x * (-rot_speed).cos() - player.camera_plane.y * (-rot_speed).sin(),
+                player.camera_plane.x * (-rot_speed).sin() + player.camera_plane.y * (-rot_speed).cos(),
+            );
+        }
     }
 }
